054551a2294dcf63b57b362379827295d68d254b
Dries Geeroms <d@driesgeeroms.com>
2013-09-18 21:53:17 +1000

    created language vars

--- cscart_pinpayments.sql
+++ cscart_pinpayments.sql
@@ -1,5 +1,4 @@
 REPLACE INTO cscart_payment_processors (processor_id, processor, processor_script,processor_template, admin_template, callback, type) values ('1005', 'Pin Payments','pin_payments.php', 'views/orders/components/payments/cc_pinpayments.tpl','pin_payments.tpl', 'Y', 'P');
 
-INSERT INTO cscart_language_values (`lang_code`, `name`, `value`)  SELECT DISTINCT lang_code, 'card_token', 'Card Token' FROM cscart_languages;
-
-# card_token, card_number, card_exp_m, card_exp_y, card_name
+-- Language Variables
+INSERT INTO cscart_language_values (`lang_code`, `name`, `value`) VALUES ('en', 'card_token', 'Card Token'), ('en', 'card_name', 'Card Name'), ('en', 'card_number', 'Card Number');
