5e5730690dd8707688f445750bfbd5c2049c7c68
Dries Geeroms <d@driesgeeroms.com>
2013-09-18 22:03:58 +1000

    mysql queries

--- cscart_pinpayments.sql
+++ cscart_pinpayments.sql
@@ -1,4 +1,11 @@
-REPLACE INTO cscart_payment_processors (processor_id, processor, processor_script,processor_template, admin_template, callback, type) values ('1005', 'Pin Payments','pin_payments.php', 'views/orders/components/payments/cc_pinpayments.tpl','pin_payments.tpl', 'Y', 'P');
+--------------------------------
+-- Install Payment Processors --
+--------------------------------
+INSERT INTO cscart_payment_processors (processor_id, processor, processor_script,processor_template, admin_template, callback, type) VALUES ('1005', 'Pin Payments','pin_payments.php', 'views/orders/components/payments/cc_pinpayments.tpl','pin_payments.tpl', 'Y', 'P');
 
--- Language Variables
+------------------------
+-- Language Variables --
+------------------------
+# Only for English... Adapt if necessary
+
 INSERT INTO cscart_language_values (`lang_code`, `name`, `value`) VALUES ('en', 'card_token', 'Card Token'), ('en', 'card_name', 'Card Name'), ('en', 'card_number', 'Card Number');
